176ed751e855755d2cd101d9d206036886b1e7ea
janghyunjun <buzz03312@naver.com>
2024-12-31 11:16:46 +0900

    Initial commit

---
+++ src/main/java/team5/team5server/Team5ServerApplication.java
@@ -0,0 +1,13 @@
+package team5.team5server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team5/team5server/Team5ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.team5server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
