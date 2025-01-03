03f505b2d6ec33f4efec134123afbad6f2e7bbfe
janghyunjun <buzz03312@naver.com>
2025-01-04 05:29:32 +0900

    bug: CORS 에러 해결

--- src/main/java/team5/team5server/global/config/WebConfig.java
+++ src/main/java/team5/team5server/global/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "http://localhost:5174") // 허용할 출처
-                .allowedMethods("GET", "POST","PATCH","DELETE") // 허용할 HTTP method
+                .allowedMethods("GET", "POST","PATCH","DELETE","OPTIONS") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
