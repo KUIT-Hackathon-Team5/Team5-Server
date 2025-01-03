242e995de96f9e209593519fd1aeaa24571b3726
janghyunjun <buzz03312@naver.com>
2025-01-03 18:27:28 +0900

    bug: CORS 에러 해결

--- src/main/java/team5/team5server/global/config/WebConfig.java
+++ src/main/java/team5/team5server/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package team5.team5server.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import team5.team5server.global.interceptor.AuthInterceptor;
@@ -18,4 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
 //                .addPathPatterns("/users/emails/verifications")
                 .addPathPatterns("/posts/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174") // 허용할 출처
+                .allowedMethods("GET", "POST","PATCH","DELETE") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+
+
 }
